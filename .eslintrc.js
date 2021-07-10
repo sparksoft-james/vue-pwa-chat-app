f40ee42351bab424bbdc766483799865ebc4c215
sparksoft-james <52090276+sparksoft-james@users.noreply.github.com>
2021-07-10 14:12:48 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,7 @@ module.exports = {
     browser: true,
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends:[
-    'prettier/vue',
-    'prettier'
-  ],
+  extends: 'standard',
   // required to lint *.vue files
   plugins: [
     'html'
